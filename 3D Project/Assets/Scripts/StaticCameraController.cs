b58f0141d381859d4cf1b6e7d13fd341547625b6
Eliza Paradise <ecp25@duke.edu>
2019-11-12 12:50:49 +0100

    working on static

---
+++ 3D Project/Assets/Scripts/StaticCameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCameraController : MonoBehaviour
+{
+
+    //public Camera myCamera;
+    public Vector3 offset;
+    public Vector3 defaultCameraPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //cameraPosition = myCamera.transform.pos
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 targetPosition = Input.mousePosition;
+            transform.position = targetPosition + offset;
+        }
+    }
+}
