159f70a4c3ff3ab4af4162418bc6179637998f57
Eliza Paradise <ecp25@duke.edu>
2019-11-18 20:00:58 +0100

    adding scene to test, working on keypad lock functionality

---
+++ 1st cam prac/Assets/Scripts/KeypadLock.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadLock : MonoBehaviour
+{
+    public GameObject door;             //GameObject that is currently in "locked" state
+
+    private string password = "ABCD";
+    private string userInput = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit cast = new RaycastHit();
+            System.Boolean objectHit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out cast);    
+            if (objectHit)
+            {
+                if (cast.transform.gameObject.name == "Keypad")     //check that user clicked on keypad
+                {
+                    //set up display of user's typing
+                    userInput += Input.inputString;
+                    print(userInput);
+                    if (Input.GetKeyDown(KeyCode.KeypadEnter))
+                    {
+                        if (userInput.Equals(password))
+                        {
+                            Unlock();
+                        }
+                        else
+                        {
+                            print("Password is incorrect.");
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+    void Unlock()
+    {
+        print("Unlocked!");
+        //delete door
+        //Instantiate new open door
+    }
+}
