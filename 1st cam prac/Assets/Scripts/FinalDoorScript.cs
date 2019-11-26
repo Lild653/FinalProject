75758a256e029dd3b5cd0fcaa378a92057ccc9f6
Eliza Paradise <ecp25@duke.edu>
2019-11-27 00:04:33 +0100

    functionality for door opening when proper code entered

---
+++ 1st cam prac/Assets/Scripts/FinalDoorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalDoorScript : MonoBehaviour
+{
+    Animator openDoorAnimate;
+
+    private void Start()
+    {
+        openDoorAnimate = GetComponent<Animator>();
+    }
+
+    public void OpenDoor()
+    {
+        openDoorAnimate.SetTrigger("OpenDoor");
+    }
+
+    //void stopAnimation()
+    //{
+    //    openDoorAnimate.enabled = false;
+    //}
+}
