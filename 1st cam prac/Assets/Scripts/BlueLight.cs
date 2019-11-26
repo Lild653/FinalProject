fc7be4f457114721d9efd5f22f6fc743b66c5df8
Danny <dasimpson@davidson.edu>
2019-11-26 18:08:57 +0100

    Fixed blue light

--- 1st cam prac/Assets/Scripts/BlueLight.cs
+++ 1st cam prac/Assets/Scripts/BlueLight.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,36 +8,48 @@ public class BlueLight : MonoBehaviour
     // Start is called before the first frame update
     public GameObject [] wireWatch;
     private Color [] properColors;
-    private System.Boolean cats = true;
+
+    private new Color renderer;
 
     void Start()
     {
-        wireWatch = new GameObject[2];
+       // wireWatch = new GameObject[2];
         properColors = new Color[2];
         properColors[0] = Color.red;
         properColors[1] = Color.green;
-       // properColors[2] = Color.black;
+//        properColors[2] = Color.black;
+    }
 
-        
+    private System.Boolean ColorChecker()
+    {
+        for (int i = 0; i < wireWatch.Length; i++)
+        {
+            renderer = wireWatch[i].GetComponent<Renderer>().material.color;
+            Mathf.FloorToInt(renderer.a);
+            if (Mathf.FloorToInt(renderer.a) != Mathf.FloorToInt(properColors[i].a) || Mathf.FloorToInt(renderer.g) != Mathf.FloorToInt(properColors[i].g))
+            { 
+                return false;
+            }
+            if(Mathf.FloorToInt(renderer.b) != Mathf.FloorToInt(properColors[i].b) || Mathf.FloorToInt(renderer.r) != Mathf.FloorToInt(properColors[i].r))
+            {
+                return false;
+            }
+        }
+        return true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        for (int i=0; i < wireWatch.Length; i++){
-            Debug.Log(wireWatch.Length);
-            if(wireWatch[i].GetComponent<Renderer>().material.color != properColors[i])
-            {
-                cats = false;
-            }
-        }
-
-        if(cats == true)
+        if (ColorChecker())
         {
-            Debug.Log("Mouse");
+            gameObject.GetComponent<Light>().enabled = true;
         }
-        
+        else
+        {
+            gameObject.GetComponent<Light>().enabled = false;
+        }
         
     }
 }
