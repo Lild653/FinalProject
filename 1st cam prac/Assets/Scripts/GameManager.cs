0490a0c825994ca6bb84170fc762b75a2de96d24
Eliza Paradise <ecp25@duke.edu>
2019-11-29 12:39:42 +0100

    adding game manager

---
+++ 1st cam prac/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private float startingSeconds = 245;
+    private float secondsRemaining;
+    public Text myText;
+    public int lastPuzzleSolved = -1;
+    //private GameObject interact;
+
+
+    void Start()
+    {
+        //interact = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        secondsRemaining = startingSeconds - Time.time;
+        int cats = Mathf.FloorToInt(secondsRemaining / 60);
+        int dogs = Mathf.FloorToInt(secondsRemaining - (cats * 60));
+        if (dogs < 10)
+        {
+            myText.text = "Time remaining: " + cats.ToString() + ":0" + dogs.ToString();
+        }
+        else
+        {
+            myText.text = "Time remaining: " + cats.ToString() + ":" + dogs.ToString();
+        }
+
+    }
+}
