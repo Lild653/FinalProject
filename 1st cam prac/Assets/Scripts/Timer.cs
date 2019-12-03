9a867de1ab7168e95653e9b638020490cd9cff16
Madubuko Chiedozie <chiedoziemadubuko@gmail.com>
2019-12-03 11:50:31 +0100

    Eliza updates

--- 1st cam prac/Assets/Scripts/Timer.cs
+++ 1st cam prac/Assets/Scripts/Timer.cs
@@ -1,37 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
-{
-    // Start is called before the first frame update
-    private float startingSeconds = 245;
-    private float secondsRemaining;
-    public Text myText;
-    //private GameObject interact;
+//public class Timer : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    //private float startingSeconds = 245;
+//    private float secondsRemaining;
+//    public Text myText;
+//    //private GameObject interact;
     
    
-    void Start()
-    {
-        //interact = gameObject;
-    }
+//    void Start()
+//    {
+//        //interact = gameObject;
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-        secondsRemaining = startingSeconds - Time.time;
-        int cats = Mathf.FloorToInt(secondsRemaining / 60);
-        int dogs = Mathf.FloorToInt(secondsRemaining - (cats * 60));
-        if (dogs < 10)
-        {
-            myText.text = "Time remaining: " + cats.ToString() + ":0" + dogs.ToString();
-        }
-        else
-        {
-            myText.text = "Time remaining: " + cats.ToString() + ":" + dogs.ToString();
-        }
+//        secondsRemaining = startingSeconds - Time.time;
+//        int cats = Mathf.FloorToInt(secondsRemaining / 60);
+//        int dogs = Mathf.FloorToInt(secondsRemaining - (cats * 60));
+//        if (dogs < 10)
+//        {
+//            myText.text = "Time remaining: " + cats.ToString() + ":0" + dogs.ToString();
+//        }
+//        else
+//        {
+//            myText.text = "Time remaining: " + cats.ToString() + ":" + dogs.ToString();
+//        }
         
-    }
-}
+//    }
+//}
