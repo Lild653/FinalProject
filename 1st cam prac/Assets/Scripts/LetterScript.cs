aad32b1a38f12680e65804b433c4f7557adf7cc0
Eliza Paradise <ecp25@duke.edu>
2019-12-04 01:27:17 +0100

    cleaning up some stuff, letter work

--- 1st cam prac/Assets/Scripts/LetterScript.cs
+++ 1st cam prac/Assets/Scripts/LetterScript.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LetterScript : MonoBehaviour
 {
     public GameObject letterCanvas;
     public GameObject gameManager;
+    public Text letterText;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +18,12 @@ public class LetterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKey(KeyCode.L) && !gameManager.GetComponent<GameManager>().inputtingText)
         {
             letterCanvas.SetActive(true);
             if (gameManager.GetComponent<GameManager>().lightsOn == true)
             {
-                //set text active too
+                letterText.text = "YoU thought you could get away with betraying me, but you won't. AFter the grounds flood you will die, all evidence will Go away, and no one will ever know what Ive done. As one final quest, Ive given you some opportunities to escape. I dont think you can figuRe it out, but Id jUST love to see you die trying. A mere door stands between you and your freedom. Its locked, of course. crack the code within three tries and youll escape. Fail, and you will meet your untimely fate. Dont worry-- Ill give you a +2 to your own funeral. The clock is ticking.";
             }
         }
     }
