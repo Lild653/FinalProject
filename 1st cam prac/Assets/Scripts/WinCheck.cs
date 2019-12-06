c4429332c0687fe384b57e85db4d92311d3dfd8f
Madubuko Chiedozie <chiedoziemadubuko@gmail.com>
2019-12-06 11:54:35 +0100

    win functionality and created new Flood script

---
+++ 1st cam prac/Assets/Scripts/WinCheck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCheck : MonoBehaviour
+{
+    void Update()
+    {
+		if (other.collider.CompareTag("Won"))
+		{
+			Destroy(other.gameObject);
+			SceneManager.LoadScene("Win");
+		}
+	}
+}
