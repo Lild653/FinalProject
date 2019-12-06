ccfcc0077f71ca05452d9050f1a416762267c399
Danny <dasimpson@davidson.edu>
2019-12-06 10:50:18 +0100

    Pause Menu

---
+++ 1st cam prac/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public Canvas pauseMenu;
+    //public GameObject gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pauseMenu.enabled && Input.GetKey(KeyCode.P))
+        {
+            pauseMenu.enabled = true;
+
+        }
+
+        if (pauseMenu.enabled)
+        {
+            if (Input.GetKey(KeyCode.P))
+            {
+                pauseMenu.enabled = false;
+            }
+        }
+    }
+}
